89f1ab5b0549c8da84cce101569cfd790af65f0a
demontoon <jeremy.fongue@yahoo.fr>
2018-08-11 18:02:56 +0200

    Mouvements basiques

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    private PlayerMotor motor;
+	
+	void Start ()
+    {
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	
+	void Update ()
+    {
+        float _x = Input.GetAxisRaw("Horizontal");
+        float _y = Input.GetAxis("Jump");
+
+        // get velocity
+        Vector2 _velocity = new Vector2(_x, _y);
+
+        motor.RunAndJump(_velocity);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerMotor : MonoBehaviour {
+    public bool _grounded;
+    private Vector2 velocity;
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float maxSpeed, maxSpeedJump, radiusCircle;
+
+    [SerializeField]
+    private GameObject GroundCheck;
+
+    [SerializeField]
+    private LayerMask layer;
+
+	void Start () {
+        velocity = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+
+	void FixedUpdate () {
+        PerformRunandJump();
+	}
+    public void RunAndJump(Vector2 _velocity)
+    {
+        velocity = _velocity;
+    }
+    private void PerformRunandJump()
+    {
+        _grounded = Physics2D.OverlapCircle(GroundCheck.transform.position,radiusCircle , layer);
+        if(_grounded)
+        {
+            rb.AddForce(new Vector2(0f, velocity.y) * Time.deltaTime * maxSpeedJump, ForceMode2D.Impulse);
+
+        }
+        rb.velocity = new Vector2(velocity.x * maxSpeed * Time.deltaTime, rb.velocity.y);
+    }
+
+}
